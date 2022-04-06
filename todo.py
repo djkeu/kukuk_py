a12916d7da80679bc95291408e045d44bff3bb35
djkeu <mrkooij@gmail.com>
2022-04-06 14:26:28 +0200

    Move items to Done-section

--- todo.py
+++ todo.py
@@ -1,15 +1,19 @@
 """
 ToDo kukuk.py
 
-- Get update_screen to fill with bg_color
-
 - Record keukuk.wav
 - Use pygame to play keukuk.wav
 - Use turtle to draw a cuckoo
-- Move settings to settings.py
 - Move cuckoo to cuckoo.py
 - Create maps for images and sounds
 - Use 'k'-key to play sound
 - Use a module to play sound every 15 minutes
+
+Done:
+- Use pygame to create a window
+- Move settings to settings.py
+- Get update_screen to fill with bg_color
+    - Had to call update_screen in run_game
+
 
 """
