fa2e553d5ff21b0664f13b7799dc74175ab5ea6f
djkeu <mrkooij@gmail.com>
2022-04-06 13:12:13 +0200

    Stop window from disappearing

--- kukuk.py
+++ kukuk.py
@@ -1,5 +1,20 @@
 # Program to mimic a cuckoo-clock
 import sys
+from turtle import bgcolor
 import pygame
 
 screen = pygame.display.set_mode((400, 300))
+
+pygame.display.set_caption("Kukuk")
+
+bg_color = (123, 234, 222)
+screen.fill(bg_color)
+
+pygame.display.flip()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
