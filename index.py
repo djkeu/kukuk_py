4736c399fc0a045c0df3af0712454354b15c19c8
djkeu <mrkooij@gmail.com>
2022-04-06 13:27:55 +0200

    Update ToDo

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 """
-kukuk.py
+ToDo kukuk.py
 - Use pygame to create a window
 - Use pygame to play the sound of a cuckoo
 - Use turtle to draw a cuckoo
--
+- Move settings to settings.py
+- Move cuckoo to cuckoo.py
+- Create maps for images and sounds
 
 """
