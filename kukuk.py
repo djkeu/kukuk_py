be02ef506fe02fa64679651a338ed785ffeab64b
djkeu <mrkooij@gmail.com>
2022-04-06 14:06:29 +0200

    Add methods to Kukuk()

--- kukuk.py
+++ kukuk.py
@@ -3,18 +3,31 @@ import pygame
 
 from settings import Settings
 
-window_size = (400, 300)
-window_caption = "Kukuk"
-bg_color = (123, 234, 222)
+class Kukuk:
+    """Class to control behaviour of the cuckoo's clock."""
 
-screen = pygame.display.set_mode(window_size)
-pygame.display.set_caption(window_caption)
-screen.fill(bg_color)
-pygame.display.flip()
+    def __init__(self) -> None:
+        """Initialize clock."""
+        pygame.init()
+        self.window_size = (400, 300)
+        self.window_caption = "Kukuk"
+        self.bg_color = (123, 234, 222)
+        self.screen = pygame.display.set_mode(self.window_size)
+        pygame.display.set_caption(self.window_caption)
 
-running = True
+    def run_clock(self):
+        """Start the loop for the clock."""
+        running = True
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+    def update_screen(self):
+        self.screen.fill(self.bg_color)
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    ku = Kukuk()
+    ku.run_clock()
