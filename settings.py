86aa952896e2a276c55a6e7ed1dddeb9a067a073
djkeu <mrkooij@gmail.com>
2022-04-06 14:32:35 +0200

    Create sceen settings section

--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 class Settings:
     """Settings for kukuk.py."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Initialize the settings."""
+        # Screen settings
         self.window_size = (400, 300)
         self.bg_color = (123, 234, 222)
         self.window_caption = "Kukuk"
