d04062e676ba3e60358311b082b5539b6e05262e
djkeu <mrkooij@gmail.com>
2022-04-06 13:47:34 +0200

    Create class Settings in settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Settings for kukuk.py."""
+
+    def __init__(self):
+        """Initialize the settings."""
+        self.window_size = (400, 300)
+        self.bg_color = (123, 234, 222)
+        self.window_caption = "Kukuk"
