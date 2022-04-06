18f245d2bbdcd33fa04818b726c16361c6d460da
djkeu <mrkooij@gmail.com>
2022-04-06 17:23:07 +0200

    Assign keukuk02.wav to kuku_sound

--- settings.py
+++ settings.py
@@ -7,3 +7,7 @@ class Settings:
         self.window_size = (400, 300)
         self.bg_color = (123, 234, 222)
         self.window_caption = "Kukuk"
+
+        # Sound setting
+        self.kuku_sound = 'sounds/keukuk02.wav'
+        
