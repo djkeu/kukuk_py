798d4ad5790b7cf624691e1254f0d6a749afee21
djkeu <mrkooij@gmail.com>
2022-04-08 17:27:39 +0200

    Copy quarterly() to minutel(), adjust settings

--- alarms.py
+++ alarms.py
@@ -2,14 +2,20 @@ from datetime import datetime
 import time
 from playsound import playsound
 
-def quarterly():
+def minute_ly():
     """Make kukuk to go off every 15 minutes."""
-    quarterly_alarms = ["00", "15", "30", "45", "15:00", "30:00", "45:00"]
+    quarterly_alarms = ["00", "30", "15", "45"]
     current_time = datetime.now().strftime("%S")
 
     if current_time in quarterly_alarms:
         time.sleep(1)
         playsound('sounds/keukuk04.wav')
         
+def quarterly():
+    """Make kukuk to go off every 15 minutes."""
+    quarterly_alarms = ["00:00", "15:00", "30:00", "45:00"]
+    current_time = datetime.now().strftime("%M:%S")
 
-quarterly()
+    if current_time in quarterly_alarms:
+        time.sleep(1)
+        playsound('sounds/keukuk04.wav')
