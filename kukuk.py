ed1bbba34d9ce7c93828e5d4c3dd25d1b128c234
djkeu <mrkooij@gmail.com>
2022-04-08 17:21:48 +0200

    Call quarterly() from kukuk

--- kukuk.py
+++ kukuk.py
@@ -37,13 +37,8 @@ class Kukuk:
                     if event.key == pygame.K_k:
                         playsound('sounds/keukuk04.wav')
 
-            quarterly_alarms = ["00", "15", "30", "45", "15:00", "30:00", "45:00"]
-            current_time = datetime.now().strftime("%S")
-
-            if current_time in quarterly_alarms:
-                time.sleep(1)
-                playsound('sounds/keukuk04.wav')
-
+            quarterly()
+            
             self.update_screen()
 
 
