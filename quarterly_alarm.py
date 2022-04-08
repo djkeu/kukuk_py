ba61dbe586e567d2c7ef812778cd4551e1ba247b
djkeu <mrkooij@gmail.com>
2022-04-08 16:08:22 +0200

    Return False to prevent infinite loop

--- quarterly_alarm.py
+++ quarterly_alarm.py
@@ -2,10 +2,12 @@ from datetime import datetime
 
 def quarterly():
     """Make kukuk to go off every 15 minutes."""
-    quarterly_alarms = [00, 15, 30, 45, "00", "05", "10","15", "30", "45"]
-    current_time = datetime.now().strftime("%M")
-    if current_time in quarterly_alarms:
-        print("Weer een kwartier voorbij!")
+    while True:
+        quarterly_alarms = [00, 15, 30, 45, "00", "04", "05", "07", "10", "15", "30", "45"]
+        current_time = datetime.now().strftime("%M")
+        if current_time in quarterly_alarms:
+            print("Weer een kwartier voorbij!")
+            return False
 
 # Test
 quarterly()
