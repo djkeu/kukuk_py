a2bcaa74137dce365bc24f5fdc91cd384e627e0f
djkeu <mrkooij@gmail.com>
2022-04-08 16:54:05 +0200

    Add seconds to alarms

--- quarterly_alarm.py
+++ quarterly_alarm.py
@@ -3,11 +3,11 @@ from playsound import playsound
 
 def quarterly():
     """Make kukuk to go off every 15 minutes."""
-    while True:
-        quarterly_alarms = ["21", "15", "30", "45"]
-        current_time = datetime.now().strftime("%M")
-        if current_time in quarterly_alarms:
-            playsound('sounds/keukuk04.wav')
-            return False
+    quarterly_alarms = ["52:00", "15:00", "30:00", "45:00"]
+    current_time = datetime.now().strftime("%M:%S")
+
+    if current_time in quarterly_alarms:
+        playsound('sounds/keukuk04.wav')
+        
 
 quarterly()
