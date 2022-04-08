df0899e13cc002f1f72f29cf51f1179911bf553a
djkeu <mrkooij@gmail.com>
2022-04-08 17:20:04 +0200

    Move code to quarterly(), import time

--- alarms.py
+++ alarms.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+import time
 from playsound import playsound
 
 def quarterly():
     """Make kukuk to go off every 15 minutes."""
-    quarterly_alarms = ["52:00", "15:00", "30:00", "45:00"]
-    current_time = datetime.now().strftime("%M:%S")
+    quarterly_alarms = ["00", "15", "30", "45", "15:00", "30:00", "45:00"]
+    current_time = datetime.now().strftime("%S")
 
     if current_time in quarterly_alarms:
+        time.sleep(1)
         playsound('sounds/keukuk04.wav')
         
 
