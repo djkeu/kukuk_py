b8fc852e95c10b42c223bebd6b2c5abb59890932
djkeu <mrkooij@gmail.com>
2022-04-08 16:09:55 +0200

    Remove integer alarm times

--- quarterly_alarm.py
+++ quarterly_alarm.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def quarterly():
     """Make kukuk to go off every 15 minutes."""
     while True:
-        quarterly_alarms = [00, 15, 30, 45, "00", "04", "05", "07", "10", "15", "30", "45"]
+        quarterly_alarms = ["00", "04", "05", "07", "10", "15", "30", "45"]
         current_time = datetime.now().strftime("%M")
         if current_time in quarterly_alarms:
             print("Weer een kwartier voorbij!")
