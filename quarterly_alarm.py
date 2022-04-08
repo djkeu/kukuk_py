7003014064759c6e1d8c09c722ee55f1e22eebe1
djkeu <mrkooij@gmail.com>
2022-04-08 16:21:03 +0200

    Change alarm test time

--- quarterly_alarm.py
+++ quarterly_alarm.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from playsound import playsound
 
 def quarterly():
     """Make kukuk to go off every 15 minutes."""
     while True:
-        quarterly_alarms = ["00", "04", "05", "07", "10", "17", "15", "30", "45"]
+        quarterly_alarms = ["21", "15", "30", "45"]
         current_time = datetime.now().strftime("%M")
         if current_time in quarterly_alarms:
             playsound('sounds/keukuk04.wav')
