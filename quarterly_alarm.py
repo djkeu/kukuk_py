0e46535f4b6b620ca26ece63dc065769b160899f
djkeu <mrkooij@gmail.com>
2022-04-08 15:55:55 +0200

    Create while loop in  quarterly()

--- quarterly_alarm.py
+++ quarterly_alarm.py
@@ -2,4 +2,11 @@ from datetime import datetime
 
 def quarterly():
     """Make kukuk to go off every 15 minutes."""
-    
+    while True:
+        quarterly_alarms = [00, 15, 30, 45, "00", "15", "30", "45"]
+        current_time = datetime.now().strftime("%M")
+        if current_time in quarterly_alarms:
+            print("Weer een kwartier voorbij!")
+
+# Test
+quarterly()
