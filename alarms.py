41954e817bed20e99b9e60f126e896845df8311b
djkeu <mrkooij@gmail.com>
2022-04-08 17:47:13 +0200

    Adjust docstrings

--- alarms.py
+++ alarms.py
@@ -3,7 +3,7 @@ import time
 from playsound import playsound
 
 def minutely():
-    """Make kukuk to go off every 15 minutes."""
+    """Make kukuk sound every 15 seconds, for testing purposes."""
     minutely_alarms = ["00", "30", "15", "45"]
     current_time = datetime.now().strftime("%S")
 
@@ -12,7 +12,7 @@ def minutely():
         playsound('sounds/keukuk04.wav')
         
 def quarterly():
-    """Make kukuk to go off every 15 minutes."""
+    """Make kukuk sound every 15 minutes."""
     quarterly_alarms = ["00:00", "15:00", "30:00", "45:00"]
     current_time = datetime.now().strftime("%M:%S")
 
