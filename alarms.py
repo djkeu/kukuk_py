1badfa8d54d7b34d39bea89b89cbd2ad8661514c
djkeu <mrkooij@gmail.com>
2022-04-10 11:25:14 +0200

    Set time.sleep to 0,3 seconds

--- alarms.py
+++ alarms.py
@@ -8,7 +8,7 @@ from settings import Settings
 def play_kuku_sound():
     """Play the kuku sound once."""
     settings = Settings()
-    time.sleep(0.2)
+    time.sleep(0.3)
     playsound(settings.kuku_sound)
 
 
