d3632e2a5ff58812be0d3a13d7290deb69a0c75f
djkeu <mrkooij@gmail.com>
2022-04-10 10:51:45 +0200

    Set alarm times and number of times  to sound

--- alarms.py
+++ alarms.py
@@ -42,5 +42,27 @@ def hourly_alarms():
     ]
     current_time = datetime.now().strftime("%H:%M:%S")
 
-    if current_time in alarms:
-        play_kuku_sound()
+    if current_time == "00:00:00" or current_time == "12:00:00":
+        play_kuku_sound() * 12
+    if current_time == "01:00:00" or current_time == "13:00:00":
+        play_kuku_sound() * 1
+    if current_time == "02:00:00" or current_time == "14:00:00":
+        play_kuku_sound() * 2
+    if current_time == "03:00:00" or current_time == "15:00:00":
+        play_kuku_sound() * 3
+    if current_time == "04:00:00" or current_time == "16:00:00":
+        play_kuku_sound() * 4
+    if current_time == "05:00:00" or current_time == "17:00:00":
+        play_kuku_sound() * 5
+    if current_time == "06:00:00" or current_time == "18:00:00":
+        play_kuku_sound() * 6
+    if current_time == "07:00:00" or current_time == "19:00:00":
+        play_kuku_sound() * 7
+    if current_time == "08:00:00" or current_time == "20:00:00":
+        play_kuku_sound() * 8
+    if current_time == "09:00:00" or current_time == "21:00:00":
+        play_kuku_sound() * 9
+    if current_time == "10:00:00" or current_time == "22:00:00":
+        play_kuku_sound() * 10
+    if current_time == "11:00:00" or current_time == "23:00:00":
+        play_kuku_sound() * 11
