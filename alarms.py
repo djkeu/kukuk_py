5e05b0f2d2f6b8927239689f8a0c5fa4f46cc6ac
djkeu <mrkooij@gmail.com>
2022-04-10 10:42:33 +0200

    Remove 00:00 from quarterly_alarms()

--- alarms.py
+++ alarms.py
@@ -23,7 +23,7 @@ def minutely_alarms():
 
 def quarterly_alarms():
     """Make kukuk sound every 15 minutes."""
-    alarms = ["00:00", "15:00", "30:00", "45:00"]
+    alarms = ["15:00", "30:00", "45:00"]
     current_time = datetime.now().strftime("%M:%S")
 
     if current_time in alarms:
