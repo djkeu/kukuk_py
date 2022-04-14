8f54a8a37bd1d7f1985c8239bd7476b77d2ca042
djkeu <mrkooij@gmail.com>
2022-04-14 11:03:13 +0200

    Add first_color to __init__

--- kukuk.py
+++ kukuk.py
@@ -14,6 +14,7 @@ class Kukuk:
 
         self.screen = pygame.display.set_mode(self.settings.window_size)
         pygame.display.set_caption(self.settings.window_caption)
+        self.first_color = self.settings.first_color
 
     def update_screen(self):
         """Update screen, flip to new screen."""
