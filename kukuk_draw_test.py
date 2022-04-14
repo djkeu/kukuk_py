fb34b2ffc8d42d8fd99ae9fd23a3b143a78a1cd7
djkeu <mrkooij@gmail.com>
2022-04-14 12:13:51 +0200

    Add loop with screen.blit()

--- kukuk_draw_test.py
+++ kukuk_draw_test.py
@@ -1,14 +1,27 @@
 import pygame
+pygame.init()
 
-def draw_lines(self):
+screen = pygame.display.set_mode((500, 500))
+first_color = (250, 250,250)
+
+def draw_lines():
     """Draw stuff with pygame.draw."""
-    self.size = (200, 200)
-    self.points = [(125, 0), (50, 125), (25, 150), (0, 125)]
+    size = (200, 200)
+    points = [(125, 0), (50, 125), (25, 150), (0, 125)]
 
-    self.lines = pygame.Surface(self.size)
-    pygame.draw.lines(self.lines, self.first_color, False, self.points)
+    lines = pygame.Surface(size)
+    pygame.draw.lines(lines, first_color, False, points)
 
-    self.lines_closed = pygame.Surface(self.size)
-    pygame.draw.lines(self.lines_closed, self.first_color, True, self.points)
+    lines_closed = pygame.Surface(size)
+    pygame.draw.lines(lines_closed, first_color, True, points)
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit()
+    
+        screen.blit(lines, (125, 125))
+        screen.blit(lines_closed, (125, 125))
+        pygame.display.update()
 
+draw_lines()
