6b12450bd883f914166687ac4efd22dda0058df1
djkeu <mrkooij@gmail.com>
2022-04-22 01:23:11 +0200

    Remove __init__ method

--- kukuk.py
+++ kukuk.py
@@ -1,13 +1,8 @@
-from settings import Settings
 from alarms import minutely_alarms, quarterly_alarms, hourly_alarms
 
 
 class Kukuk():
     """Class to control behaviour of the cuckoo's clock."""
-
-    def __init__(self) -> None:
-        """Initialize clock."""
-        self.settings = Settings()
 
     def run_clock(self):
         """Start the loop for the clock."""
