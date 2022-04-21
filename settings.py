5ef41980ebdf91d6788c19ef9cff0d796de47b45
djkeu <mrkooij@gmail.com>
2022-04-21 13:13:19 +0200

    Restore Screen settings

--- settings.py
+++ settings.py
@@ -3,5 +3,10 @@ class Settings:
 
     def __init__(self) -> None:
         """Initialize the settings."""
+        # Screen settings
+        self.window_size = (400, 300)
+        self.bg_color = (101,67,33)
+        self.window_caption = "Kukuk"
+
         # Sound settings
         self.kuku_sound = 'sounds/keukuk03.wav'
