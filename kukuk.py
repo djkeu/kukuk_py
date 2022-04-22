f046f9ff01c24730fcf0b8d6e1c4d7e87741bfc6
djkeu <mrkooij@gmail.com>
2022-04-22 14:57:30 +0200

    Paste and adjust code from kivykuk.py

--- kukuk.py
+++ kukuk.py
@@ -1,19 +1,30 @@
-from alarms import minutely_alarms, quarterly_alarms, hourly_alarms
+import kivy
+kivy.require('2.1.0')
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.clock import Clock
+
+from alarms import quarterly_alarms, hourly_alarms
 
 
-class Kukuk():
+class Kukuk(Label):
     """Class to control behaviour of the cuckoo's clock."""
 
-    def run_clock(self):
-        """Start the loop for the clock."""
-        running = True
+    text = "Kukuk kukuk"
+    text += "\n\n\t\t\t Kukuk \t\t\t"
 
-        while running:
-            quarterly_alarms()
-            hourly_alarms()
-            minutely_alarms()
+    def my_callback(dt):
+        quarterly_alarms()
+        hourly_alarms()
+    event = Clock.schedule_interval(my_callback, 1 / 30)
+
+
+class KukukApp(App):
+
+    def build(self):
+        return Kukuk()
 
 
 if __name__ == '__main__':
-    ku = Kukuk()
-    ku.run_clock()
+    KukukApp().run()
