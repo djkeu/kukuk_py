2786fd3cd9a6e5d2957e8868c17686e7ca1147b3
djkeu <mrkooij@gmail.com>
2022-04-25 20:17:58 +0200

    Change 'alarms' in quarterly_alarms()  to a set

--- alarms.py
+++ alarms.py
@@ -20,7 +20,7 @@ def times_kuku(times):
 
 def quarterly_alarms():
     """Play kuku sound every 15 minutes."""
-    alarms = ["15:00", "30:00", "45:00"]
+    alarms = ("15:00", "30:00", "45:00")
     current_time = datetime.now().strftime("%M:%S")
 
     if current_time in alarms:
