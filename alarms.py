7f14a27b76006ad80f0ccaaf7c0b2527da568962
djkeu <mrkooij@gmail.com>
2022-04-28 20:56:14 +0200

    Switch back to keukuk03.wav

--- alarms.py
+++ alarms.py
@@ -6,7 +6,7 @@ from kivy.core.audio import SoundLoader
 
 def play_kuku_sound():
     """Play kuku sound once."""
-    kuku_sound = SoundLoader.load('sounds/keukuk06.wav')
+    kuku_sound = SoundLoader.load('sounds/keukuk03.wav')
     time.sleep(1)
     kuku_sound.play()
 
