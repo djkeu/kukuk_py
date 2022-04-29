2b5ee5603effd8acca1cc7b488fe50a03785452f
djkeu <mrkooij@gmail.com>
2022-04-29 21:22:20 +0200

    callback once every 5 seconds

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class KukukApp(App):
     def alarms_callback(dt):
         quarterly_alarms()
         hourly_alarms()
-    event = Clock.schedule_interval(alarms_callback, 1 / 30)
+    event = Clock.schedule_interval(alarms_callback, 5)
 
 
 if __name__ == '__main__':
