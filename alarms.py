22d365b58568eda7e32fbdceae49d37bc2c5fa5e
djkeu <mrkooij@gmail.com>
2022-04-30 00:05:29 +0200

    Update docstring of minutely_alarms()

--- alarms.py
+++ alarms.py
@@ -43,7 +43,7 @@ def hourly_alarms():
 
 
 def minutely_alarms():
-    """Sound alarm every ten seconds, for testing purposes."""
+    """Sound alarm every five seconds, for testing purposes."""
     current_time = datetime.now().strftime("%S")
     intervals = (00, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55)
 
