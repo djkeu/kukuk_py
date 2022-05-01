11461cc4d987d636052d202ced0645b8a84cdc6c
djkeu <mrkooij@gmail.com>
2022-05-01 17:39:51 +0200

    pep8

--- alarms.py
+++ alarms.py
@@ -4,6 +4,7 @@ from datetime import datetime
 # from kivy.core.audio import SoundLoader
 from playsound import playsound
 
+
 def kuku_once():
     """Play kuku sound once."""
     kuku_sound = 'sounds/keukuk03.wav'
