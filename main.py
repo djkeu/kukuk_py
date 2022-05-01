fd7961394fed19556a49274089ef6503da676441
djkeu <mrkooij@gmail.com>
2022-05-01 14:43:28 +0200

    Add class Klok to display time

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ kivy.require('2.1.0')
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 from kivy.clock import Clock
 
+from datetime import datetime
 from alarms import quarterly_alarms, hourly_alarms
 
 
@@ -14,8 +16,14 @@ class BoxLayoutKukuk(BoxLayout):
     pass
 
 
+class Klok(Label):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.now = datetime.now()
+        self.my_label.text = self.now.strftime("%H:%M:%M")
+
+
 class KukukApp(App):
-   
     def alarms_callback(dt):
         quarterly_alarms()
         hourly_alarms()
