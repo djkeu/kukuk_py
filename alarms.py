b04afb6cbd17f855d374d88a989f2d5768cd617d
djkeu <mrkooij@gmail.com>
2022-07-15 22:02:40 +0200

    Improve readability quarterly_alarms()

--- alarms.py
+++ alarms.py
@@ -14,8 +14,12 @@ def kuku_once():
 
 def quarterly_alarms():
     """ Play kuku sound every 15 minutes. """
-    alarms = ("15:00", "30:00", "45:00")
     current_time = datetime.now().strftime("%M:%S")
+    alarms = (
+        "15:00", 
+        "30:00", 
+        "45:00"
+        )
 
     if current_time in alarms:
         kuku_once()
@@ -23,8 +27,12 @@ def quarterly_alarms():
 
 def test_quarterly_alarms():
     """ Play kuku sound every 15 minutes. """
-    alarms = ("15:00", "30:00", "45:00")
     current_time = datetime.now().strftime("%M:%S")
+    alarms = (
+        "15:00", 
+        "30:00", 
+        "45:00"
+        )
 
     if current_time in alarms:
         kuku_once()
