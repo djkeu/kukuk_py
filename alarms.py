67024fa33426f6972f34a9c8de07591a251beb6d
djkeu <mrkooij@gmail.com>
2022-07-18 20:15:41 +0200

    Remove test_alarms()

--- alarms.py
+++ alarms.py
@@ -22,16 +22,6 @@ def quarterly_alarms():
         print(f"Alarm sounded at: {current_quarterly_time}")
 
 
-def test_quarterly_alarms():
-    """Play kuku sound every 15 minutes."""
-    current_secondly_time = datetime.now().strftime("%S.%f")[: -5]
-    alarms = ("06.2", "16.2", "26.2", "36.2", "46.2", "56.2")
-
-    if current_secondly_time in alarms:
-        kuku_once()
-        print(current_secondly_time)
-
-
 def kuku_times(times):
     """ Play kuku sound multiple times in a row. """
     for i in range(0, times):
@@ -53,30 +43,3 @@ def hourly_alarms():
         if current_hourly_time == hour:
             kuku_times(times)
             print(f"Hourly alarms sounded {times} times at: {current_hourly_time}")
-
-
-def test_hourly_alarms():
-    """ Play kuku sound according to the minutes. """
-    # For testing purposes only
-    current_minutely_time = datetime.now().strftime("%M:%S")
-
-    for i in range(1, 60):
-        if i < 11:
-            times = i
-        elif i < 21:
-            times = i - 10
-        elif i < 31:
-            times = i - 20
-        elif i < 41:
-            times = i - 30
-        elif i < 51:
-            times = i - 40
-        else:
-            times = i - 50
-
-        minute = f"{i:02}:00"
-        if current_minutely_time == minute:
-            print(f"Minute: {minute}")
-            print(f"Current time: {current_minutely_time}")
-            print(f"Times:  {times}")
-            kuku_times(times)
