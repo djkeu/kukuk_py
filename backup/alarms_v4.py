bf429b9996db71263c161f820974ee0d8cda6079
djkeu <mrkooij@gmail.com>
2022-07-18 20:14:43 +0200

    Create backup files_v4

---
+++ backup/alarms_v4.py
@@ -0,0 +1,82 @@
+import time
+from datetime import datetime
+
+from kivy.core.audio import SoundLoader
+
+
+def kuku_once():
+    """ Play kuku sound once. """
+    kuku_sound = 'sounds/keukuk03.wav'
+
+    sound = SoundLoader.load(kuku_sound)
+    sound.play()
+ 
+
+def quarterly_alarms():
+    """Play kuku sound every 15 minutes."""
+    current_quarterly_time = datetime.now().strftime("%M:%S.%f")[: -5]
+    alarms = ("15:00.1", "30:00.1", "45:00.1")
+
+    if current_quarterly_time in alarms:
+        kuku_once()
+        print(f"Alarm sounded at: {current_quarterly_time}")
+
+
+def test_quarterly_alarms():
+    """Play kuku sound every 15 minutes."""
+    current_secondly_time = datetime.now().strftime("%S.%f")[: -5]
+    alarms = ("06.2", "16.2", "26.2", "36.2", "46.2", "56.2")
+
+    if current_secondly_time in alarms:
+        kuku_once()
+        print(current_secondly_time)
+
+
+def kuku_times(times):
+    """ Play kuku sound multiple times in a row. """
+    for i in range(0, times):
+        kuku_once()
+        time.sleep(1.1)
+
+
+def hourly_alarms():
+    """ Play kuku sound according to the hours. """
+    current_hourly_time = datetime.now().strftime("%H:%M:%S")
+
+    for i in range(1, 25):
+        if i < 13:
+            times = i
+        else:
+            times = (i - 12)
+
+        hour = f"{i:02}:00:00"
+        if current_hourly_time == hour:
+            kuku_times(times)
+            print(f"Hourly alarms sounded {times} times at: {current_hourly_time}")
+
+
+def test_hourly_alarms():
+    """ Play kuku sound according to the minutes. """
+    # For testing purposes only
+    current_minutely_time = datetime.now().strftime("%M:%S")
+
+    for i in range(1, 60):
+        if i < 11:
+            times = i
+        elif i < 21:
+            times = i - 10
+        elif i < 31:
+            times = i - 20
+        elif i < 41:
+            times = i - 30
+        elif i < 51:
+            times = i - 40
+        else:
+            times = i - 50
+
+        minute = f"{i:02}:00"
+        if current_minutely_time == minute:
+            print(f"Minute: {minute}")
+            print(f"Current time: {current_minutely_time}")
+            print(f"Times:  {times}")
+            kuku_times(times)
---
+++ backup/main_v4.py
@@ -0,0 +1,39 @@
+__version__ = "0.1.5"
+
+import kivy
+
+kivy.require('2.1.0')
+
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+
+from time import strftime
+from alarms import quarterly_alarms 
+from alarms import hourly_alarms
+# from alarms import test_quarterly_alarms
+# from alarms import test_hourly_alarms
+
+
+class BoxLayoutKukuk(BoxLayout):
+    pass
+
+
+class KukukApp(App):
+    def update_time(self, i_val):
+        self.root.ids.time.text = strftime("%H:%M:%S")
+
+    def on_start(self):
+        Clock.schedule_interval(self.update_time, 1)
+
+    def alarms_callback(dt):
+        quarterly_alarms()
+        hourly_alarms()
+        # test_quarterly_alarms()
+        # test_hourly_alarms()
+    
+    event = Clock.schedule_interval(alarms_callback, 1 / 10)
+
+
+if __name__ == '__main__':
+    KukukApp().run()
