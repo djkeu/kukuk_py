15b00af617bf6a6da67142c753c91621c0c389f7
djkeu <mrkooij@gmail.com>
2022-07-21 13:33:16 +0200

    Make print statement more informative

--- alarms.py
+++ alarms.py
@@ -54,10 +54,12 @@ def quarterly_alarms():
 def test_quarterly_alarms():
     """Play kuku sound every 5 seconds."""
     current_minutely_time = datetime.now().strftime("%S.%f")[: -5]
+    current_logged_time = datetime.now().strftime("%M:%S:%f")[: -5]
+    
     alarms = ("01.2", "06.2", "11.2", "16.2", "21.2", "26.2", 
             "31.2", "36.2", "41.2", "46.2", "51.2", "56.2")
 
     if current_minutely_time in alarms:
         kuku_once()
-        print(f"Test quarterly alarm sounded at: {current_minutely_time}")
+        print(f"Test quarterly alarm sounded at: {current_logged_time}")
 
