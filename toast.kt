7fed7d3007b51cfb135fcb0d5db0090bc7e4fe7e
djkeu <mrkooij@gmail.com>
2023-02-17 12:25:09 +0100

    Backup Toast from kukuk-android

---
+++ toast.kt
@@ -0,0 +1,11 @@
+// import android.widget.Toast
+
+
+        /*
+        val kukuToast = Toast.makeText(applicationContext,
+            getString((R.string.kukukTextView)),
+            Toast.LENGTH_LONG)
+
+        kukuToast.show()
+        */
+
